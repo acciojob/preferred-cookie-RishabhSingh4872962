bd5c6e126a738ba9364465d5b50cabfb8f01cdfa
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-24 22:45:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const size=document.getElementById("fontsize");
+const color=document.getElementById("fontcolor");
+
+
+document.cookies={size:size.value,color:color.value};
+	console.log(document.cookies);
+document.body.style.color=`${document.cookies.color}`; 
+document.body.style.fontSize=`${document.cookies.size}px`; 
+
+function handleSubmit() {
+	this.event.preventDefault();  
+	document.cookies={size:size.value,color:color.value};
+	console.log(document.cookies);
+document.body.style.color=`${document.cookies.color}`; 
+document.body.style.fontSize=`${document.cookies.size}px`; 
+		console.log(document.cookies.size);
+}
